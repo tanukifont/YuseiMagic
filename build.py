584a84aeb5ee91505b575e56c2e33d93d5381d98
Aaron <aaron@sajatypeworks.com>
2021-06-08 10:55:12 -0700

    Hinting removed
    Per GF request, all hinting has been removed and GASP table set to smoothing.

--- build.py
+++ build.py
@@ -10,6 +10,13 @@ import os
 print ("Converting to UFO")
 main(("glyphs2ufo", "sources/YuseiMagic.glyphs"))
 
+def GASP_set(font:TTFont):
+    if "gasp" not in font:
+        font["gasp"] = newTable("gasp")
+        font["gasp"].gaspRange = {}
+    if font["gasp"].gaspRange != {65535: 0x000A}:
+        font["gasp"].gaspRange = {65535: 0x000A}
+
 exportFont = ufoLib2.Font.open("sources/YuseiMagic-Regular.ufo")
 
 exportFont.lib['com.github.googlei18n.ufo2ft.filters'] = [{
@@ -27,19 +34,10 @@ static_ttf["DSIG"].usNumSigs = 0
 static_ttf["DSIG"].signatureRecords = []
 static_ttf["head"].flags |= 1 << 3        #sets flag to always round PPEM to integer
 
+GASP_set(static_ttf)
+
 print ("[Yusei Magic] Saving")
 static_ttf.save("fonts/ttf/YuseiMagic-Regular.ttf")
 
 shutil.rmtree("sources/YuseiMagic-Regular.ufo")
 os.remove("sources/YuseiMagic.designspace")
-
-subprocess.check_call(
-        [
-            "ttfautohint",
-            "--stem-width",
-            "nsn",
-            "fonts/ttf/YuseiMagic-Regular.ttf",
-            "fonts/ttf/YuseiMagic-Regular-hinted.ttf",
-        ]
-    )
-shutil.move("fonts/ttf/YuseiMagic-Regular-hinted.ttf", "fonts/ttf/YuseiMagic-Regular.ttf")
